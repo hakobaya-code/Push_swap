e50962eaec5992e089488975eac3f9d983d0e05c
Haruka Kobayashi <hakobaya@c1r21s7.42tokyo.jp>
2023-11-12 15:44:19 +0900

    add header

--- push_swap.h
+++ push_swap.h
@@ -6,11 +6,16 @@
 /*   By: hakobaya <hakobaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 15:26:25 by hakobaya          #+#    #+#             */
-/*   Updated: 2023/10/23 18:12:15 by hakobaya         ###   ########.fr       */
+/*   Updated: 2023/11/12 15:43:39 by hakobaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "libft/libft.h"
+
+#endif
