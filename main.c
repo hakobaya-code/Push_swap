f5f5f68e60e12c3eb427a0224abff92e96665cf1
Haruka Kobayashi <hakobaya@c1r21s7.42tokyo.jp>
2023-11-12 15:50:24 +0900

    add main.c

--- main.c
+++ main.c
@@ -6,9 +6,26 @@
 /*   By: hakobaya <hakobaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 15:37:52 by hakobaya          #+#    #+#             */
-/*   Updated: 2023/11/12 15:38:01 by hakobaya         ###   ########.fr       */
+/*   Updated: 2023/11/12 15:49:53 by hakobaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (0);
+	else
+	{
+		void(argv);
+	}
+	return (0);
+}
+
+
+ __attribute__((destructor))
+ static void destructor()
+ {
+ 	system("leaks -q push_swap");
+ }
