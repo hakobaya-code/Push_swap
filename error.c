50dbed93226561dfcc6c64256954f2ad67235906
Haruka Kobayashi <hakobaya@c1r24s1.42tokyo.jp>
2023-12-09 13:21:37 +0900

    change atoi

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: hakobaya <hakobaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 14:25:36 by hakobaya          #+#    #+#             */
-/*   Updated: 2023/12/08 11:38:01 by hakobaya         ###   ########.fr       */
+/*   Updated: 2023/12/09 13:14:13 by hakobaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	error_notdigit(void)
 {
-	ft_putstr_fd("ERROR: ARGV IS NOT DIGIT !!", 1);
+	ft_putstr_fd("🙅‍♀️ERROR: ARGV IS NOT DIGIT !!", 1);
 	return ;
 }
 
 int	error_argc(void)
 {
-	ft_putstr_fd("ERROR:AT LEAST TWO ARGUMENTS ARE REQUIRED.", 1);
+	ft_putstr_fd("🙅‍♀️ERROR:AT LEAST TWO ARGUMENTS ARE REQUIRED.", 1);
 	return (1);
 }
 
